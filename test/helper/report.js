d75dc7d62e85c60a2a6ea3099bd95c3ed6ede3f8
Mark Wubben <mark@novemberborn.net>
2018-02-15 13:40:09 +0000

    Add integration tests for reporters
    This replaces the previous reporter tests. Instead we run the reporter
    for varying test outputs and record the results. The environment is
    modified to ensure the highest fidelity colors and figures are used.
    Where necessary output is rewritten so the resulting logs can be
    compared across Node.js versions and operating systems.
    
    `./test/helpers/replay.js` can be used to replay a log file.
    
    Code coverage is still pretty good, though. I'm about to refactor the
    reporters so any missing coverage will be resolved at that point.

---
+++ test/helper/report.js
@@ -0,0 +1,141 @@
+'use strict';
+const childProcess = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const globby = require('globby');
+const proxyquire = require('proxyquire');
+const replaceString = require('replace-string');
+const Logger = require('../../lib/logger');
+
+let _Api = null;
+const createApi = options => {
+	if (!_Api) {
+		_Api = proxyquire('../../api', {
+			'./lib/fork': proxyquire('../../lib/fork', {
+				child_process: Object.assign({}, childProcess, { // eslint-disable-line camelcase
+					fork(filename, argv, options) {
+						return childProcess.fork(path.join(__dirname, 'report-worker.js'), argv, options);
+					}
+				})
+			})
+		});
+	}
+
+	return new _Api(options);
+};
+
+// At least in Appveyor with Node.js 6, IPC can overtake stdout/stderr
+let hasReliableStdIO = true;
+exports.captureStdIOReliability = () => {
+	if (process.platform === 'win32' && parseInt(process.versions.node, 10) < 8) {
+		hasReliableStdIO = false;
+	}
+};
+
+exports.assert = (t, logFile, buffer, stripStdIO) => {
+	let existing = null;
+	try {
+		existing = fs.readFileSync(logFile);
+	} catch (err) {}
+	if (existing === null || process.env.UPDATE_REPORTER_LOG) {
+		fs.writeFileSync(logFile, buffer);
+		existing = buffer;
+	}
+
+	let expected = existing.toString('utf8');
+	// At least in Appveyor with Node.js 6, IPC can overtake stdout/stderr. This
+	// causes the reporter to emit in a different order, resulting in a test
+	// failure. "Fix" by not asserting on the stdout/stderr reporting at all.
+	if (stripStdIO && !hasReliableStdIO) {
+		expected = expected.replace(/---tty-stream-chunk-separator\n(stderr|stdout)\n/g, '');
+	}
+	t.is(buffer.toString('utf8'), expected);
+};
+
+exports.sanitizers = {
+	cwd: str => replaceString(str, process.cwd(), '~'),
+	posix: str => replaceString(str, '\\', '/'),
+	slow: str => str.replace(/(slow.+?)\(\d+m?s\)/g, '$1 (000ms)'),
+	// TODO: Remove when Node.js 4 support is dropped
+	stacks: str => str.replace(/(\[90m|')t \((.+?\.js:\d+:\d+)\)/g, '$1$2').replace(/Test\.t \[as fn\]/g, 'Test.fn').replace(/null\._onTimeout/g, 'Timeout.setTimeout'),
+	// At least in Appveyor with Node.js 6, IPC can overtake stdout/stderr. This
+	// causes the reporter to emit in a different order, resulting in a test
+	// failure. "Fix" by not asserting on the stdout/stderr reporting at all.
+	unreliableProcessIO(str) {
+		return !hasReliableStdIO && (str === 'stdout\n' || str === 'stderr\n') ? '' : str;
+	}
+};
+
+const run = (type, reporter) => {
+	const projectDir = path.join(__dirname, '../fixture/report', type.toLowerCase());
+
+	const api = createApi({
+		failFast: type === 'failFast' || type === 'failFast2',
+		failWithoutAssertions: false,
+		serial: type === 'failFast' || type === 'failFast2',
+		require: [],
+		cacheEnable: true,
+		compileEnhancements: true,
+		explicitTitles: type === 'watch',
+		match: [],
+		babelConfig: {testOptions: {}},
+		resolveTestsFrom: projectDir,
+		projectDir,
+		timeout: undefined,
+		concurrency: 1,
+		updateSnapshots: false,
+		snapshotDir: false,
+		color: true
+	});
+
+	reporter.api = api;
+	const logger = new Logger(reporter);
+	logger.start();
+
+	api.on('test-run', runStatus => {
+		reporter.api = runStatus;
+		runStatus.on('test', logger.test);
+		runStatus.on('error', logger.unhandledError);
+
+		runStatus.on('stdout', logger.stdout);
+		runStatus.on('stderr', logger.stderr);
+	});
+
+	const files = globby.sync('*.js', {cwd: projectDir}).sort();
+	if (type !== 'watch') {
+		return api.run(files).then(runStatus => {
+			logger.finish(runStatus);
+		});
+	}
+
+	// Mimick watch mode
+	return api.run(files).then(runStatus => {
+		logger.finish(runStatus);
+
+		// Don't clear
+		logger.reset();
+		logger.section();
+		logger.reset();
+		logger.start();
+
+		return api.run(files);
+	}).then(runStatus => {
+		runStatus.previousFailCount = 2;
+		logger.finish(runStatus);
+
+		// Clear
+		logger.clear();
+		logger.reset();
+		logger.start();
+
+		return api.run(files);
+	}).then(runStatus => {
+		logger.finish(runStatus);
+	});
+};
+
+exports.regular = reporter => run('regular', reporter);
+exports.failFast = reporter => run('failFast', reporter);
+exports.failFast2 = reporter => run('failFast2', reporter);
+exports.only = reporter => run('only', reporter);
+exports.watch = reporter => run('watch', reporter);
